63c1e20604b0fca7860eef4c6a628f2dff78372c
Govardhan Pagidi <govardhan.pagidi@honeywell.com>
2018-08-22 15:23:24 +0530

    Added console app to test easily if required

---
+++ ParseXMLTagsConsoleApp/src/ConsoleApp2/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string xmlstring = @"Hi Yvaine,
+                            Please create an expense claim for the below. Relevant details are marked up as requested…
+                            <expense><cost_centre>DEV002</cost_centre> <total>890.55</total><payment_method>personal
+                            card</payment_method>
+                            </expense>
+                            From: Ivan Castle
+                            Sent: Friday, 16 February 2018 10:32 AM
+                            To: Antoine Lloyd <Antoine.Lloyd@example.com>
+                            Subject: test
+                            Hi Antoine,
+                            Please create a reservation at the <vendor>Viaduct Steakhouse</vendor> our <description>development
+                            team’s project end celebration dinner</description> on <date>Tuesday 27 April 2017</date>. We expect to
+                            arrive around 7.15pm. Approximately 12 people but I’ll confirm exact numbers closer to the day.
+                            Regards,
+                            Ivan
+                            Your";
+            string validXML = GetMatch(xmlstring);
+
+            string xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><root>";
+            xml += validXML;
+            xml += "</root>";
+            XDocument xmlRoot = null;
+            ApiError error = new ApiError();
+            if (!string.IsNullOrEmpty(xml))
+            {
+                if (IsValidRequest(xml, ref xmlRoot, ref error))
+                {
+                    var result = UpdateXMLwithDefalutValues(xmlRoot.Element("root"));
+                    Console.WriteLine(result);
+                    Dictionary<string, string> values = new Dictionary<string, string>();
+                    foreach (var item in result.Elements())
+                    {
+                        if (!values.ContainsKey(item.Name.LocalName))
+                        {
+                            values.Add(item.Name.LocalName, item.Value);
+                            Console.WriteLine(item.Name.LocalName +":"+ item.Value);
+                        }
+                    }
+                    Console.ReadKey();
+                }
+            }
+            Console.ReadKey();
+
+        }
+
+
+        private static string GetMatch(string text)
+        {
+            Regex RegexTagsPattern = new Regex(@"<[^>]+>[^<]*</[^>]+>");
+            StringBuilder output = new StringBuilder();
+
+            bool matchFound = false;
+            foreach (var item in RegexTagsPattern.Matches(text))
+            {
+                matchFound = true;
+                output.Append(item);
+            }
+
+            if (matchFound)
+                return output.ToString();
+            else
+                return string.Empty;
+        }
+
+        private static bool IsValidRequest(string xml, ref XDocument xmlRoot, ref ApiError error)
+        {
+            try
+            {
+                //Load the given string into xml document object
+                xmlRoot = XDocument.Parse(xml);
+
+                List<string> mandatoryElements = new List<string> { "total" };
+                foreach (var elementName in mandatoryElements)
+                {
+                    // XML should have total element in it.
+                    if (!xmlRoot.Descendants(elementName).Any())
+                    {
+                        error.ErrorCode = 1402;
+                        error.ErrorMessage = $"{elementName} tag is missing in the request!";
+                        Console.WriteLine(error.ErrorMessage);
+                        return false;
+                    }
+                }
+
+                //All validations passed
+                return true;
+            }
+            catch (Exception ex)
+            {
+                error.ErrorCode = 1401;
+                error.ErrorMessage = "Input XML is not formatted correctly, please check and try again !";
+                Console.WriteLine("Input data is incorrect! Your Request is rejected!");
+            }
+            return true;
+        }
+
+        private static XElement UpdateXMLwithDefalutValues(XElement xmlRoot)
+        {
+            if (!xmlRoot.Elements("cost_centre").Any())
+            {
+                XElement csElement = new XElement("cost_centre", "UNKNOWN");
+                xmlRoot.Add(csElement);
+            }
+            return xmlRoot;
+        }
+
+    }
+    class ApiError
+    {
+        public string ErrorMessage { get; set; }
+        public int ErrorCode { get; set; }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ReadingXMLTags
 Created a sample api to read the xml content which is part of string content
+
+Note: To Open the solution we need Visualstudio 2015(update 3)
+we can use postman to test the api
+
+
+To make things easier to test follow the below link once you run api by pressing F5
+
+http://localhost:54900/api/parser?text=Hi%20Yvaine,%20Please%20create%20an%20expense%20claim%20for%20the%20below.%20Relevant%20details%20are%20marked%20up%20as%20requested%E2%80%A6%20%3Cexpense%3E%3Ccost_centre%3EDEV002%3C/cost_centre%3E%20%3Ctotal%3E890.55%3C/total%3E%3Cpayment_method%3Epersonal%20card%3C/payment_method%3E%20%3C/expense%3E%20From:%20Ivan%20Castle%20Sent:%20Friday,%2016%20February%202018%2010:32%20AM%20To:%20Antoine%20Lloyd%20%3CAntoine.Lloyd@example.com%3E%20Subject:%20test%20Hi%20Antoine,%20Please%20create%20a%20reservation%20at%20the%20%3Cvendor%3EViaduct%20Steakhouse%3C/vendor%3E%20our%20%3Cdescription%3Edevelopment%20team%E2%80%99s%20project%20end%20celebration%20dinner%3C/description%3E%20on%20%3Cdate%3ETuesday%2027%20April%202017%3C/date%3E.%20We%20expect%20to%20arrive%20around%207.15pm.%20Approximately%2012%20people%20but%20I%E2%80%99ll%20confirm%20exact%20numbers%20closer%20to%20the%20day.%20Regards,%20Ivan%20Your
